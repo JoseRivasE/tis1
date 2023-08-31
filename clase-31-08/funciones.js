7f44b5d08cc3fd97f77d39724093e6599d9ea513
José Rivas <jrivas@ing.ucsc.cl>
2023-08-31 11:48:51 -0400

    tis clase 31-08-23
    cover y javascript

---
+++ clase-31-08/funciones.js
@@ -0,0 +1,10 @@
+console.log("holaaaaaaaaaaa");
+function azul() {
+    document.getElementById("azul").style.backgroundColor = "blue";
+}
+function verde(){
+    document.getElementsByClassName("verde")[0].style.backgroundColor = "green";
+}
+function reset(){
+    document.getElementById("azul").style.backgroundColor = "red";
+}
